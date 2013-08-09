345d5a49aa490ffeeb4003cc9e20fe314a3bf095
basvandenberg <b.a.vandenberg@gmail.com>
2013-08-09 21:48:56 +0200

    Making installable package

---
+++ README.txt
@@ -0,0 +1,6 @@
+=====
+biopy
+=====
+
+The biopy package is sort of Swiss Army knife for bioinformaticians, albeit
+still a rather small one.
---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='biopy',
+    version='0.1.0',
+    author='B.A. van den Berg',
+    author_email='b.a.vandenberg@gmail.com',
+    packages=['biopy'],
+    package_dir={'biopy': 'biopy'},
+    package_data={'biopy': ['data/*/*']},
+    url='http://pypi.python.org/pypi/biopy/',
+    license='LICENSE.txt',
+    description='A little Swiss Army knife for bioinformaticians',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy >= 1.7.1",
+        "scipy >= 0.12.0",
+        "matplotlib >= 1.2.2"
+    ]
+)
