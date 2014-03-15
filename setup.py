c1338b111af154226a632e993d1d19d6c38a486b
basvandenberg <b.a.vandenberg@gmail.com>
2014-03-15 10:37:45 +0100

    Forgotten update version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='biopy',
-    version='0.1.0',
+    version='0.1.1',
     author='B.A. van den Berg',
     author_email='b.a.vandenberg@gmail.com',
     packages=['biopy'],
